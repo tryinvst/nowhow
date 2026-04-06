fa95354030268a761e9517ab3a53e53453372e13
TariffsFamilyTest+spaceadd <vbinder@alfabank.ru>
2026-04-06 15:14:08 +0300

    <ELK> updated logs + add new services

---
+++ demo-app-repository-feature-firts-log/elkModule/src/main/java/demo/controller/LogController.java
@@ -0,0 +1,24 @@
+package demo.controller;
+
+import com.example.demo.module.LogRequest;
+import com.example.demo.service.ElasticSearchService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@RestController
+@RequestMapping("/api/logs")
+public class LogController {
+    private static final Logger log = LoggerFactory.getLogger(LogController.class);
+    private final ElasticSearchService elasticSearchService;
+
+    public LogController(ElasticSearchService elasticSearchService) {
+        this.elasticSearchService = elasticSearchService;
+    }
+
+    @PostMapping
+    public void logOnly(@RequestBody LogRequest logRequest) {
+        log.info("Пришел запрос: {}", logRequest.getMessage());
+
+        elasticSearchService.logEvent(logRequest.getMessage());
+    }
+}
