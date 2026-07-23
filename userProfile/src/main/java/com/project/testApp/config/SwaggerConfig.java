23cd88fd2a9c98a31ff9263b4c66dd395e590ec0
TariffsFamilyTest+spaceadd <vbinder@alfabank.ru>
2026-07-23 15:35:09 +0300

    <ModulesUpdates> -add docs in openapi.yaml

--- userProfile/src/main/java/com/project/testApp/config/SwaggerConfig.java
+++ userProfile/src/main/java/com/project/testApp/config/SwaggerConfig.java
@@ -2,11 +2,18 @@ package com.project.testApp.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
 @OpenAPIDefinition(info = @Info(title="api"))
 public class SwaggerConfig {
+    @Bean
+public OpenAPI customOpenApi () {
+    return new OpenAPI().info (new io.swagger.v3.oas.models.info.Info().title("мой API")
+      .version ( "1.0.0" )
+      .description("документация"));
 
-
+}
 }
